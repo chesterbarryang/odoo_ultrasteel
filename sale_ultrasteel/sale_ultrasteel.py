b8a2894bbab6de4e43f533600bcd222520871147
chesterbarryang <chester.ang@gmail.com>
2016-01-31 13:18:04 +0800

    use pricelist price to compare amount during SO

--- sale_ultrasteel/sale_ultrasteel.py
+++ sale_ultrasteel/sale_ultrasteel.py
@@ -1,12 +1,17 @@
 from openerp import api, fields, models, _
 from openerp import tools
 from openerp.tools.translate import _
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class sale_ultrasteel(models.Model):
     _inherit = "sale.order.line"
 
     def check_margin(self, cr, uid, ids, product_id, unit_price, context=None):
-      res = {}
+        res = {}
+
+        _logger.debug('Start: check_margin()')
       # warning = {}
       # sale_price = None
       # if self.order_id.pricelist_id and self.order_id.partner_id:
@@ -40,6 +45,7 @@ class sale_ultrasteel(models.Model):
     @api.multi
     @api.onchange('price_unit')
     def price_unit_change(self):
+        _logger.debug('Start: price_unit_change')
         if not self.product_id:
             return {'domain': {'product_uom': []}}
 
@@ -57,6 +63,8 @@ class sale_ultrasteel(models.Model):
             uom=self.product_uom.id
         )
 
+        _logger.debug('Product: %s', product)
+
         name = product.name_get()[0][1]
         if product.description_sale:
             name += '\n' + product.description_sale
@@ -66,6 +74,8 @@ class sale_ultrasteel(models.Model):
 
         if self.order_id.pricelist_id and self.order_id.partner_id:
             vals['price_unit'] = self.env['account.tax']._fix_tax_included_price(product.price, product.taxes_id, self.tax_id)
+
+        _logger.debug('Update before return: %s', vals)
         self.update(vals)
         return {'domain': domain}
 
