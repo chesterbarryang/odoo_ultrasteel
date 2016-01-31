44405b6c645eabfcf21b8b05f68afb39f668f6fa
chesterbarryang <chester.ang@gmail.com>
2016-01-31 12:09:03 +0800

    use pricelist price to compare amount during SO

--- sale_ultrasteel/sale_ultrasteel.py
+++ sale_ultrasteel/sale_ultrasteel.py
@@ -9,8 +9,18 @@ class sale_ultrasteel(osv.Model):
       res = {}
       warning = {}
       sale_price = None
+      product = self.product_id.with_context(
+                lang=self.order_id.partner_id.lang,
+                partner=self.order_id.partner_id.id,
+                quantity=self.product_uom_qty,
+                date_order=self.order_id.date_order,
+                pricelist=self.order_id.pricelist_id.id,
+                uom=self.product_uom.id
+            )
+
       if product_id:
-        sale_price = self.pool.get('product.product').browse(cr, uid,product_id).list_price
+        # sale_price = self.pool.get('product.product').browse(cr, uid,product_id).list_price
+        sale_price = self.env['account.tax']._fix_tax_included_price(product.price, product.taxes_id, self.tax_id)
       if unit_price is None:
         pass
       elif unit_price < sale_price:
