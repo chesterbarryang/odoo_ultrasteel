b4ffc3929d369f00b7bb1c8af6c64a228603fd3d
chesterbarryang <chester.ang@gmail.com>
2016-02-11 17:18:55 +0800

    add new fields for cheque payment

---
+++ account_bank_payment/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ultrasteel Cheque Payment Addons",
+    "summary": "Additional fields for cheque payments",
+    'description': """
+Add new fields if cheque payment:
+1. Bank Name
+2. Bank Branch
+3. Cheque Number
+    """,
+    'version': "1.0",
+    'author': "Chester Ang",
+    'category': "Tools",
+    'depends': ['account'],
+    'data': [],
+    'demo': [],
+    'installable': True
+}
---
+++ account_bank_payment/account_bank_payment.py
@@ -0,0 +1,14 @@
+from openerp import api, fields, models, _
+from openerp import tools
+from openerp.tools.translate import _
+
+class account_bank_payment(models.Model):
+    _name = "account.bank.payments"
+    _inherit = ['account.abstract.payment']
+    _description = "Capture additional fields for cheque payments"
+
+    x_bank_name = fields.Char(string='Bank',help='Bank name')
+    x_bank_branch = fields.Char(string='Branch',help='Bank branch name')
+    x_cheque_no = fields.Char(string='Cheque No.',help='Cheque number')
+
+account_bank_payment()
