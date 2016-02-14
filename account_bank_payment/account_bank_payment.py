976ee2221c5c9cb8044b62c7d0cc4ed05a85ce1d
chesterbarryang <chester.ang@gmail.com>
2016-02-14 17:27:48 +0800

    Show only bank fields if payment type is cheque

--- account_bank_payment/account_bank_payment.py
+++ account_bank_payment/account_bank_payment.py
@@ -6,8 +6,9 @@ class account_bank_payment(models.Model):
     _inherit = ['account.payment']
     _description = "Capture additional fields for cheque payments"
 
-    x_bank_name = fields.Char(string='Bank',help='Bank name')
-    x_bank_branch = fields.Char(string='Branch',help='Bank branch name')
-    x_cheque_no = fields.Char(string='Cheque No.',help='Cheque number')
+    x_bank_name = fields.Char(string='Bank',help='Bank name. For cheque payments')
+    x_bank_branch = fields.Char(string='Branch',help='Bank branch name. For cheque payments')
+    x_cheque_no = fields.Char(string='Cheque No.',help='Cheque number. For cheque payments')
+    x_payment_type = fields.Char(related='journal_id.type')
 
 account_bank_payment()
