786bfe43eaf5a57a02fd9c792c4691ed8edf63a4
chesterbarryang <chester.ang@gmail.com>
2016-02-14 18:03:22 +0800

    Show only bank fields if payment type is cheque

--- account_bank_payment/account_bank_payment.py
+++ account_bank_payment/account_bank_payment.py
@@ -3,7 +3,6 @@ from openerp import tools
 from openerp.tools.translate import _
 
 class account_bank_payment(models.Model):
-    _name ='account_bank_payments'
     _inherit = ['account.payment']
     _description = "Capture additional fields for cheque payments"
 
