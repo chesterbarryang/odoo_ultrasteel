47d793dc47e9b522fe48458b10571f5a7bcb3eb2
chesterbarryang <chester.ang@gmail.com>
2016-04-29 18:06:51 +0800

    initial: Account Payable Billing Approval fix deployment errors

--- account_ultrasteel/wizard/account_report_billing_approval.py
+++ account_ultrasteel/wizard/account_report_billing_approval.py
@@ -21,8 +21,8 @@ class AccountBillingApproval(models.TransientModel):
         data['ids'] = self.env.context.get('active_ids', [])
         data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
         data['form'] = self.read(['partner_id'])[0]
-        used_context = self._build_contexts(data)
-        data['form']['used_context'] = dict(used_context, lang=self.env.context.get('lang', 'en_US'))
+        #used_context = self._build_contexts(data)
+        #data['form']['used_context'] = dict(used_context, lang=self.env.context.get('lang', 'en_US'))
         return self._print_report(data)
 
     def _print_report(self, data):
