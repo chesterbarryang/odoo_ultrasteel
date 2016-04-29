5d78a2401a5c3f4d91f0e6f0cfd95545537c0556
chesterbarryang <chester.ang@gmail.com>
2016-04-30 01:46:42 +0800

    initial: Account Payable Billing Approval fix: cannot get partner_id value

--- account_ultrasteel/wizard/account_report_billing_approval.py
+++ account_ultrasteel/wizard/account_report_billing_approval.py
@@ -27,19 +27,22 @@ class AccountBillingApproval(models.TransientModel):
         data = {}
         data['ids'] = self.env.context.get('active_ids', [])
         data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
+
         data['form'] = self.read(['partner_id'])[0]
         used_context = self._build_contexts(data)
         data['form']['used_context'] = dict(used_context, lang=self.env.context.get('lang', 'en_US'))
 
+        _logger.error('START')
         _logger.error('self partner ids:', self.partner_id.name)
         _logger.error('ids:', tuple(data['ids']))
         _logger.error('model:', tuple(data['model']))
         _logger.error('form:', tuple(data['form']))
         _logger.error('self partner ids:', tuple(data['model']))
+        _logger.error('END')
 
         return self._print_report(data)
 
     def _print_report(self, data):
-        _logger.error('partner value:', self.read(['partner_id'])[0].name)
+        _logger.error('partner value:', self.read(['partner_id'])[0].id)
         data['form'].update(self.read(['partner_id'])[0])
         return self.env['report'].get_action(self, 'account_ultrasteel.report_billingapproval', data=data)
