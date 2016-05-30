c0752ec3bc79d5ce742f7237e3bf3db7efdad970
chesterbarryang <chester.ang@gmail.com>
2016-05-30 20:29:45 +0800

    Account Payable Billing Approval

--- account_ultrasteel/models/account_invoice_ultrasteel.py
+++ account_ultrasteel/models/account_invoice_ultrasteel.py
@@ -26,7 +26,23 @@ class account_invoice_ultrasteel(models.Model):
     #              " * The 'Cancelled' status is used when user cancel invoice.")
     # }
     #
-    state = fields.Selection(selection_add=[('approve', "Approve")])
+    #state = fields.Selection(selection_add=[('approve', "Approve")])
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('proforma', 'Pro-forma'),
+        ('proforma2', 'Pro-forma'),
+        ('open', 'Open'),
+        ('approve', "Approve")
+        ('paid', 'Paid'),
+        ('cancel', 'Cancelled'),
+    ], string='Status', index=True, readonly=True, default='draft',
+        track_visibility='onchange', copy=False,
+        help=" * The 'Draft' status is used when a user is encoding a new and unconfirmed Invoice.\n"
+             " * The 'Pro-forma' status is used the invoice does not have an invoice number.\n"
+             " * The 'Open' status is used when user create invoice, an invoice number is generated. Its in open status till user does not approve invoice.\n"
+             " * The 'Approve' status will remain until user pays the invoice"
+             " * The 'Paid' status is set automatically when the invoice is paid. Its related journal entries may or may not be reconciled.\n"
+             " * The 'Cancelled' status is used when user cancel invoice.")
 
     @api.one
     def action_approve_payment(self):
